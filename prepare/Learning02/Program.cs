9d623b226b4e148bf143c64c51ca74fbfe5c1575
Aurora Norlund <auroranorlund@gmail.com>
2024-11-05 20:04:56 -0700

    w2 prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2018;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Senior Web Developer";
+        job2._startYear = 2012;
+        job2._endYear = 2017;
+
+        Resume resume = new Resume();
+        resume._name = "John Doe";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        Console.WriteLine(resume._jobs[0]._jobTitle);
+
+        resume.DisplayResume();
     }
 }
